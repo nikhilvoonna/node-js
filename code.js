1367da507853e00c4cccb6329bcd8b13f47d9205
nikhilvoonna <nikhilvoonna@gmail.com>
2020-09-25 18:11:48 +0530

    changes

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 //js basics
 const person = {
     name : "nikhil",
-    age : 24
+    age : 24,
+    occupation : "develo[per"
 }
 console.log(person);
---
+++ home/code.js
@@ -0,0 +1,6 @@
+const person = {
+    name : "akhil",
+    age : 20,
+    occupation : "study"
+}
+console.log(person);
